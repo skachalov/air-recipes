238d025d670268b4844155993193b5f90fe590cd
Sergey <sergey.kachalov.02@gmail.com>
2022-09-28 14:11:59 +0700

    recipes-2-main-page-my-input: Create MyInput component

--- air-recipes/src/main.js
+++ air-recipes/src/main.js
@@ -1,7 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import * as components from 'vuetify/lib/components'
+import * as directives from 'vuetify/lib/directives'
+import "vuetify/lib/styles/main.css"
+import "@mdi/font/css/materialdesignicons.css"
+import {createVuetify} from "vuetify";
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        iconfont: 'mdi'
+    }
+})
+
 
 loadFonts()
 

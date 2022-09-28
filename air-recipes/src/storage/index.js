e870adaafa622b21ebf63257a88e5ce9cac42fce
Sergey <sergey.kachalov.02@gmail.com>
2022-09-28 10:48:15 +0700

    recipes-1-vuex: Add modal module to storage modules

--- air-recipes/src/storage/index.js
+++ air-recipes/src/storage/index.js
@@ -1,13 +1,16 @@
 import {createStore} from "vuex";
 import recipes from "@/storage/recipes";
 import recipe from "@/storage/recipe";
+import modal from "@/storage/modal";
 import RecipesViewModel from "@/model/recipesViewModel";
 import RecipeViewModel from "@/model/recipeViewModel";
+import ModalViewModel from "@/model/modalViewModel";
 
 const store = createStore({
     modules: {
         recipes,
-        recipe
+        recipe,
+        modal
     }
 })
 
@@ -18,6 +21,7 @@ class ViewModelSingleton {
             ViewModelSingleton._instance = this
             this._recipesViewModal = new RecipesViewModel(store)
             this._recipeViewModal = new RecipeViewModel(store)
+            this._modalViewModel = new ModalViewModel(store)
         }
     }
     getRecipesViewModal() {
@@ -25,6 +29,9 @@ class ViewModelSingleton {
     }
     getRecipeViewModal() {
         return this._recipeViewModal
+    }
+    getModalViewModel() {
+        return this._modalViewModel
     }
 }
 

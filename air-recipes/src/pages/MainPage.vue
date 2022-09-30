7f34f0257b3b0027ea56a6c56211a7ddc07fdf7c
Sergey <sergey.kachalov.02@gmail.com>
2022-09-30 16:06:09 +0700

    recipes-2-main-page-storage: Add getting data from vuex

--- air-recipes/src/pages/MainPage.vue
+++ air-recipes/src/pages/MainPage.vue
@@ -1,7 +1,30 @@
 <template>
   <my-header />
+  <v-container fluid class="ps-16 pe-16">
+    <v-row class="pt-8 justify-center">
+        <base-card
+            v-for="recipe in store.getters.getRecipes"
+            :key="recipe.id"
+            :recipe="recipe"
+        />
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
   import MyHeader from "@/components/MainPage/BaseHeader/BaseHeader";
+  import BaseCard from "@/components/MainPage/BaseCard/BaseCard"
+  import { onMounted } from "vue";
+  import { useStore } from "vuex"
+  import {viewModel} from "@/storage";
+
+  const store = useStore()
+
+  onMounted(() => {
+    viewModel.getRecipesViewModal().fetchRecipes()
+  })
 </script>
+
+<style scoped>
+
+</style>

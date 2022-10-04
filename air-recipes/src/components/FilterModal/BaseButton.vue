4a47998bf594a64bb654aec0f665e8004473791b
Sergey <sergey.kachalov.02@gmail.com>
2022-10-04 12:20:31 +0700

    recipes-3-filter-modal-base-button: Create BaseButton component

---
+++ air-recipes/src/components/FilterModal/BaseButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-btn
+      :class="{ dark: isDark, light: !isDark }"
+      variant="outlined"
+  >
+    <slot />
+  </v-btn>
+</template>
+
+<script setup>
+  import { defineProps } from "vue"
+
+  defineProps({
+    isDark: Boolean
+  })
+</script>
+
+<style scoped>
+  .dark {
+    background-color: #82786A;
+    color: white;
+  }
+  .light {
+    border-color: #82786A;
+    color: #82786A;
+  }
+</style>

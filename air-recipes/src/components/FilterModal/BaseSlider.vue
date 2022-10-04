39423ae702d8ba553b82185650084e4ba14986af
Sergey <sergey.kachalov.02@gmail.com>
2022-10-04 13:07:25 +0700

    recipes-3-filter-modal-base-slider: Create BaseSlider component

---
+++ air-recipes/src/components/FilterModal/BaseSlider.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-range-slider
+      class="slider"
+      color="#82786A"
+      thumb-size="12px"
+      thumb-label="always"
+      track-size="2px"
+      :min="range.min"
+      :max="range.max"
+      :model-value="modelValue"
+      @update:modelValue="updateInput"
+      step="5"
+  >
+  </v-range-slider>
+</template>
+
+<script setup>
+  import { defineProps, defineEmits } from "vue"
+
+  defineProps({
+    range: Object,
+    modelValue: Array
+  })
+
+  const emit = defineEmits(["update:modelValue"])
+
+  function updateInput(event) {
+    emit("update:modelValue", event)
+  }
+</script>

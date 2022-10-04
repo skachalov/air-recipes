b4e979cadfff33da3e4e6d477605b27c15ac56b9
Sergey <sergey.kachalov.02@gmail.com>
2022-10-04 13:55:59 +0700

    recipes-3-filter-modal-base-checkbox: Start work with BaseCeckbox component

---
+++ air-recipes/src/components/FilterModal/BaseCheckbox.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="base-checkbox">
+    <label class="text"><slot /></label>
+    <v-checkbox class="checkbox" />
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+  .base-checkbox {
+    width: 376px;
+    height: 47px;
+    padding-bottom: 11px;
+    border-bottom: 1px solid rgba(33, 33, 33, 0.08);
+    display: flex;
+    justify-content: space-between;
+  }
+  .text {
+    display: flex;
+    align-items: end;
+    height: 42px;
+  }
+  .checkbox {
+    display: flex;
+    justify-content: end;
+    align-items: start;
+  }
+</style>

d1633bbfaedc297f2cba8e6088e39d321d3949a1
Sergey <sergey.kachalov.02@gmail.com>
2022-10-05 13:33:18 +0700

    recipes-3-filter-modal: Make opening and closing modal window

---
+++ air-recipes/src/components/FilterModal/FilterModal.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container
+      class="container"
+      @click.self="closeModal"
+      fluid
+  >
+    <div class="modal"></div>
+  </v-container>
+</template>
+
+<script setup>
+  import { defineEmits } from "vue"
+
+  const emits = defineEmits(['closeModal'])
+
+  function closeModal() {
+    emits('closeModal')
+  }
+</script>
+
+<style scoped>
+  .container {
+    position: fixed;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    z-index: 3;
+    background-color: rgba(255, 255, 255, 0.8);
+  }
+  .modal {
+    position: fixed;
+    height: 558px;
+    width: 440px;
+    background-color: white;
+    z-index: 4;
+    box-shadow: 0px 16px 24px 0px rgba(0, 0, 0, 0.14);
+  }
+</style>
---
+++ air-recipes/src/components/FilterModal/FilterModalController.vue
@@ -0,0 +1,22 @@
+<template>
+  <filter-modal
+      v-show="store.state.modal.isShownModal"
+      @closeModal="closeModal"
+  />
+</template>
+
+<script setup>
+  import FilterModal from "@/components/FilterModal/FilterModal"
+  import { useStore } from "vuex"
+  import { viewModel } from "@/storage"
+
+  const store = useStore()
+
+  function closeModal() {
+    viewModel.getModalViewModel().switchIsShownModal()
+  }
+</script>
+
+<style scoped>
+
+</style>

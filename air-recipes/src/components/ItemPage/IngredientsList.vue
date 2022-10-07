fb34070a0f71d182450c67bc1607769d92c0d7bd
Sergey <sergey.kachalov.02@gmail.com>
2022-10-07 12:45:01 +0700

    recipes-4-item-page-ingredients-list: Create IngredientsList component

---
+++ air-recipes/src/components/ItemPage/IngredientsList.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-row class="flex-column">
+    <v-row class="ingredients-title mb-6">
+      <h3>Ingredients</h3>
+    </v-row>
+    <v-row class="ingredients-list">
+      <ul>
+        <li
+          v-for="ingredient in ingredients"
+          :key="ingredient"
+          class="mb-3"
+        >
+          {{ ingredient }}
+        </li>
+      </ul>
+    </v-row>
+  </v-row>
+</template>
+
+<script setup>
+  import { defineProps } from "vue"
+
+  defineProps({
+    ingredients: Array
+  })
+</script>
+
+<style scoped>
+  .ingredients-title {
+    font-size: 24px;
+  }
+</style>

9f6b753fa731b9b6ce38601cb192da4b04d39a50
Sergey <sergey.kachalov.02@gmail.com>
2022-10-07 13:13:25 +0700

    recipes-4-item-page-instructions-list: Create InstructionsList component

---
+++ air-recipes/src/components/ItemPage/InstructionsList/InstructionItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <li class="mb-3 li-item">
+    <div class="count-bullet">{{ idx + 1 }}</div>
+    {{ instruction }}
+  </li>
+</template>
+
+<script setup>
+  import { defineProps } from "vue"
+
+  defineProps({
+    instruction: String,
+    idx: Number
+  })
+</script>
+
+<style scoped>
+  .li-item {
+    display: flex;
+    align-items: center;
+    list-style-type: none;
+  }
+ .count-bullet {
+   width: 16px;
+   height: 16px;
+   border: 1px solid #DDDDDD;
+   border-radius: 50%;
+   margin-right: 8px;
+   display: flex;
+   justify-content: center;
+   font-size: 9px;
+   font-weight: 700;
+ }
+</style>

b24468b501e90400588e927ef57ead92e82d3efd
Sergey <sergey.kachalov.02@gmail.com>
2022-10-12 11:39:23 +0700

    recipes-5-refactoring: Make animation on cards

--- air-recipes/src/pages/MainPage.vue
+++ air-recipes/src/pages/MainPage.vue
@@ -3,15 +3,15 @@
     <v-container class="ps-16 pe-16" fluid>
       <base-progress v-if="!$store.state.recipes.recipes.length" />
       <v-row
-          v-else
           class="pt-8 justify-center"
       >
-        <transition name=""></transition>
-        <base-card
-            v-for="recipe in getRecipes"
-            :key="recipe.id"
-            :recipe="recipe"
-        />
+        <transition-group name="base-cards">
+          <base-card
+              v-for="recipe in getRecipes"
+              :key="recipe.id"
+              :recipe="recipe"
+          />
+        </transition-group>
       </v-row>
     </v-container>
   </v-container>
@@ -26,3 +26,19 @@
     getRecipes: Array
   })
 </script>
+
+<style scoped>
+  .base-cards-item {
+    display: inline-block;
+  }
+  .base-cards-enter-active,
+  .base-cards-leave-active {
+    transition: all .7s ease;
+  }
+  .base-cards-enter-from,
+  .base-cards-leave-to {
+    opacity: 0;
+    transform: translateY(-30px);
+  }
+
+</style>

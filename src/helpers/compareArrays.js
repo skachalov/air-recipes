6a1c423615fbe56d4285e07b52839eb39dc98f10
spoonty <sergey.kachalov.02@gmail.com>
2022-10-18 16:01:57 +0700

    resipes-6-testing: Create tests for compareArrays function

--- src/helpers/compareArrays.js
+++ src/helpers/compareArrays.js
@@ -1,3 +1,4 @@
 export function compareArrays(arr1, arr2) {
-    return arr1.filter(el => arr2.indexOf(el) !== -1).length === arr2.length
+    return arr1.length === arr2.length
+        && arr1.filter(el => arr2.indexOf(el) !== -1).length === arr2.length
 }
